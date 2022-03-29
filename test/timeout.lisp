155e4c1352de02b1e611896c34fca00823edd808
hxz <herongxian@163.com>
2022-03-29 14:06:35 +0800

    test timeout

--- test/timeout.lisp
+++ test/timeout.lisp
@@ -1,1 +1,6 @@
 (in-package :timer-wheel-tests)
+
+(define-test inspect-bt-timeout-context :parent timeout
+  (let ((context (tw::make-bt-context)))
+    (log:info "context: ~d" context)
+    (finish (tw::inspect-bt-timeout-context context))))
