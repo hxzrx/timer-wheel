c0207b7b3543016d59dfe78f66f0fbfe72ee04b1
hxz <herongxian@163.com>
2022-03-29 11:11:15 +0800

    calculate curret ms without the help of local-time:now

--- test/utils.lisp
+++ test/utils.lisp
@@ -1,1 +1,56 @@
 (in-package :timer-wheel-tests)
+
+(log:debug :debug)
+
+(define-test positive :parent utils
+  (of-type timer-wheel::positive-real 1)
+  (of-type timer-wheel::positive-fixnum 1)
+  (false (typep 0 'timer-wheel::positive-real))
+  (false (typep 0 'timer-wheel::positive-fixnum)))
+
+(define-test current-universal-milliseconds :parent utils
+  (let ((now0 (tw::get-current-universal-milliseconds))
+        (now1 (tw::current-universal-milliseconds))
+        (now2 (tw::timestamp->universal-milliseconds (local-time:now))))
+    (log:debug "now0: ~d, now1: ~d, now2: ~d" now0 now1 now2)
+    (true (<= (- now2 now0) 1))
+    (true (<= (- now2 now1) 1))))
+
+(define-test get-local-timezone :parent utils
+  (is = 8 (tw::get-local-timezone)))
+
+(define-test timestring->timestamp :parent utils
+  (let ((ts1 "2022-03-24 16:28:00")
+        (ts2 "2022-03-24T16:28:00")
+        (ts3 "2022-03-24T16:28:00+08:00")
+        (ts4 "2022-03-24T16:28:00.123456+08:00"))
+    (finish (tw::timestring->timestamp ts1))
+    (finish (tw::timestring->timestamp ts2))
+    (finish (tw::timestring->timestamp ts3))
+    (finish (tw::timestring->timestamp ts4))
+    (log:debug "timestring: ~d, timestamp: ~d" ts1 (tw::timestring->timestamp ts1))
+    (log:debug "timestring: ~d, timestamp: ~d" ts2 (tw::timestring->timestamp ts2))
+    (log:debug "timestring: ~d, timestamp: ~d" ts3 (tw::timestring->timestamp ts3))
+    (log:debug "timestring: ~d, timestamp: ~d" ts4 (tw::timestring->timestamp ts4))))
+
+(define-test timestamp->timestring :parent utils
+  (let ((now (local-time:now)))
+    (finish (tw::timestamp->timestring now))
+    (log:debug "timestamp: ~d, timestring: ~d" now (tw::timestamp->timestring now))))
+
+(define-test timestamp->universal-milliseconds :parent utils
+  (let ((now1 (local-time:now))
+        (now2 (tw::get-current-universal-milliseconds)))
+    (finish (tw::timestamp->universal-milliseconds now1))
+    (true (<= (- now2 (tw::timestamp->universal-milliseconds now1)) 1))))
+
+(define-test timestring->universal-milliseconds :parent utils
+  (let* ((now-ms (tw::get-current-universal-milliseconds))
+         (timestamp (tw::universal-milliseconds->timestamp now-ms))
+         (timestring (tw::timestamp->timestring timestamp)))
+    (is = now-ms (tw::timestring->universal-milliseconds timestring))))
+
+(define-test timestamp->universal-milliseconds :parent utils
+  (let* ((timestamp (local-time:now))
+         (now-ts (tw::get-current-universal-milliseconds)))
+    (true (<= (- now-ts (tw::timestamp->universal-milliseconds timestamp)) 1))))
