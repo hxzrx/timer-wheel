b3bc5953a1a67e3cfde45140eed751e0fd4abe1e
hxz <herongxian@163.com>
2022-04-02 14:07:56 +0800

    verify scheduler in threads

---
+++ examples/verify-thread.lisp
@@ -0,0 +1,28 @@
+;;;; Keep an atomic counter structure, and increase it atomically in the callback of the timer.
+;;;; Invoke tw:schedule-timer in threads.
+;;;; Check the counter finally.
+
+(in-package #:timer-wheel.examples)
+
+(defparameter *atomic-counter* (tw::make-atomic 0))
+
+(defparameter *verify-wheel* (tw:make-wheel))
+
+(defun gen-timer ()
+  (tw:make-timer :callback (lambda (wheel timer)
+                             (declare (ignore wheel timer))
+                             (tw::atomic-incf (tw::atomic-place *atomic-counter*)))
+                 :scheduler *verify-wheel*))
+
+(defun verify-scheduler-threads (threads-num)
+  "Schedule timer in threads, check if some timers lost or not."
+  (defparameter *atomic-counter* (tw::make-atomic 0))
+  (dotimes (i threads-num)
+    (bt:make-thread #'(lambda ()
+                        (tw:schedule-timer *verify-wheel* (gen-timer)))))
+  (sleep (* threads-num 0.0001))
+  (if (= threads-num (tw::atomic-place *atomic-counter*))
+      (format t "PASSED!~%")
+      (format t "FAILED~%")))
+
+(verify-scheduler-threads 10000)
