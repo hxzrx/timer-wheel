d92a72d26d45fc1241df13e5f90f0ceac50111e8
hxz <herongxian@163.com>
2022-04-07 10:21:13 +0800

    optimize

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,8 +1,9 @@
 (in-package #:timer-wheel)
 
-(defconstant +milliseconds-per-second+ 1000)
-
 (log:debug :debug)
+
+(defconstant +milliseconds-per-second+ 1000)
+(defconstant +unix-epoch+ (encode-universal-time 0 0 0 1 1 1970 0))
 
 (deftype positive-real ()
   '(real (0) *))
@@ -24,21 +25,38 @@
 (declaim (inline current-universal-milliseconds))
 (defun get-current-universal-milliseconds ()
   "Get the universal time, in millisecond"
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
   (multiple-value-bind (sec nsec) (local-time::%get-current-time)
-    (+ (* (+ sec (encode-universal-time 0 0 0 1 1 1970 0))
-          +milliseconds-per-second+)
-       (truncate (/ nsec 1000000)))))
+    (the fixnum (+ (the fixnum (* (+ (the fixnum sec) +unix-epoch+)
+                                  +milliseconds-per-second+))
+                   (truncate (/ (the fixnum nsec) 1000000))))))
+
+#+:ignore
+(defun get-current-universal-milliseconds () ; slow
+  "Get the universal time, in millisecond"
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (let* ((now (local-time:now))
+         (msec (the fixnum (/ (the fixnum (local-time:nsec-of now)) 1000))))
+    (the fixnum (+ (the fixnum (* (the fixnum (local-time:timestamp-to-universal now))
+                                  +milliseconds-per-second+))
+                   msec))))
 
 (declaim (inline get-current-universal-milliseconds))
 (defun current-universal-milliseconds ()
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
   (get-current-universal-milliseconds))
 
+(declaim (inline get-local-timezone))
 (defun get-local-timezone ()
   "Get time zone number of the local machine,
 plus number for the eastern time zones and minus number for the western time zones.
 So, the time zone of Beijing is 8."
-  (- (nth-value 8 (decode-universal-time (get-universal-time)))))
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (- (the fixnum (nth-value 8 (decode-universal-time (get-universal-time))))))
 
+(defconstant +local-timezone+ (get-local-timezone))
+
+(declaim (inline timestring->timestamp))
 (defun timestring->timestamp (timestring)
   "Parse a timestring to a timestamp object with the time zone converted to the local zone.
 The timestring should have at least two parts: date and time, concatenated with #\T or #\Space.
@@ -47,18 +65,19 @@ See the examples below."
   ;; (timestring->timestamp "2022-03-24T16:28:00")
   ;; (timestring->timestamp "2022-03-24T16:28:00+08:00")
   ;; (timestring->timestamp "2022-03-24T16:28:00.123456+08:00")
-  (declare (string timestring))
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
   (local-time:parse-timestring timestring
                                :fail-on-error t
-                               :date-time-separator (cond ((char= (aref timestring 10) #\T) #\T)
-                                                          ((char= (aref timestring 10) #\Space) #\Space)
-                                                          ((char= (aref timestring 10) #\t) #\t)
+                               :date-time-separator (cond ((char= (schar timestring 10) #\T) #\T)
+                                                          ((char= (schar timestring 10) #\Space) #\Space)
+                                                          ((char= (schar timestring 10) #\t) #\t)
                                                           (t (error "Invalid timestring format: ~d" timestring)))
                                :allow-missing-date-part nil
                                :allow-missing-time-part nil
                                :allow-missing-timezone-part t
-                               :offset (* (get-local-timezone) 60 60)))
+                               :offset (* +local-timezone+ 60 60)))
 
+(declaim (inline timestamp->timestring))
 (defun timestamp->timestring (timestamp &key (format *default-time-format*) (timezone local-time:*default-timezone*))
   "Convert a timestamp object to a timestring, default iso-3339 format and local time zone."
   ;; (timestamp-timestring (local-time:now))
@@ -66,23 +85,29 @@ See the examples below."
   (declare (local-time:timestamp timestamp))
   (local-time:format-timestring nil timestamp :format format :timezone timezone))
 
+(declaim (inline timestamp->universal-milliseconds))
 (defun timestamp->universal-milliseconds (timestamp)
   "Convert a timestamp object to universal time, in milliseconds"
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
   (declare (local-time:timestamp timestamp))
-  (+ (* (local-time:timestamp-to-universal timestamp) +milliseconds-per-second+)
-     (local-time:timestamp-millisecond timestamp)))
+  (the fixnum (+ (the fixnum (* (the fixnum (local-time:timestamp-to-universal timestamp)) +milliseconds-per-second+))
+                 (the fixnum (local-time:timestamp-millisecond timestamp)))))
 
+(declaim (inline timestring->universal-milliseconds))
 (defun timestring->universal-milliseconds (timestring)
   "Convert a timestring to universal time, in milliseconds"
   ;; (timestring->universal-milliseconds (timestamp->timestring (local-time:now)))
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
   (declare (string timestring))
-  (let ((timestamp (timestring->timestamp timestring)))
-    (+ (* (local-time:timestamp-to-universal timestamp) +milliseconds-per-second+)
-       (local-time:timestamp-millisecond timestamp))))
+  (let ((timestamp (the local-time:timestamp (timestring->timestamp timestring))))
+    (the fixnum (+ (the fixnum (* (the fixnum (local-time:timestamp-to-universal timestamp)) +milliseconds-per-second+))
+                   (the fixnum (local-time:timestamp-millisecond timestamp))))))
 
+(declaim (inline universal-milliseconds->timestamp))
 (defun universal-milliseconds->timestamp (universal)
   "Convert a universal time (in milliseconds) to a timestamp object."
-  (multiple-value-bind (sec msec) (floor universal 1000)
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (multiple-value-bind (sec msec) (floor (the fixnum universal) 1000)
     (local-time:universal-to-timestamp sec :nsec (* msec 1000000))))
 
 (defmacro when-let (bindings &body forms) ; alexandria's when-let
